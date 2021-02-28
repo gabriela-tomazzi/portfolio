95133093332ce530b9c8d963a2d6f2c2ed1dcfc5
gabriela-tomazzi <gabtomazzi@gmail.com>
2021-02-28 11:08:05 -0300

    hero responsive and traducted

--- script.js
+++ script.js
@@ -5,4 +5,34 @@ let i = 0;
 function changeLanguage() {
 	button.innerText = langs[i];
 	i == 1 ? i = 0 : i++;
+
+	if ( i == 1) {
+		portHero();
+		portAbout();
+		portProducts();
+	} else {
+		engHero();
+	}
+	
+}
+
+function portHero() {
+	let h1 = document.querySelector('.hero h1');
+	h1.innerHTML = '<h1>Olá, eu sou <span class="text_destaque">Gabi</span>.</h1>';
+	
+	let p = document.querySelector('.hero p');
+	p.innerHTML = '<p>E eu sou uma <br> <span class="text_destaque">Desenvolvedora </span>  full stack!</p>';
+}
+function engHero() {
+	let h1 = document.querySelector('.hero h1');
+	h1.innerHTML = '<h1>Hi, I am <span class="text_destaque">Gabi</span>.</h1>';
+
+	let p = document.querySelector('.hero p');
+	p.innerHTML = '<p>I am a full stack <span class="text_destaque"> developer</span>!</p>';
+}
+function portAbout() {
+
+}
+function portProducts() {
+
 }
