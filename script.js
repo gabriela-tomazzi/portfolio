786cdaf9d61c52f052e6665711c326e97d414204
gabriela-tomazzi <gabtomazzi@gmail.com>
2021-03-28 11:38:39 -0300

    another redesign

--- script.js
+++ script.js
@@ -1,50 +1,13 @@
-let text_change = document.getElementById('changeText');
+let modeSwitcher = document.querySelector('#switcher');
+modeSwitcher.addEventListener('click', switchMode)
+d = 0
 
-const target = document.querySelectorAll('[data-anime]');
-
-let words = [
-	'Developer.',
-	'Designer.',
-	'Gabi!'
-]
-let count = 0;	//keeps track of words
-let index = 0;	//keeps track of letters
-let currentText = '';
-let letter = '';
-
-function typeWritter() {
-	if(count === words.length) {
-		count = 0;
+function switchMode() {
+	if(d == 0) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		d = 1;
+	} else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		d = 0;
 	}
-
-	currentText = words[count];
-	letter = currentText.slice(0, ++index);		//letter is sliced 0, 0 then 0, 1 0, 2 ...
-	text_change.textContent = letter;
-
-	if(letter.length === currentText.length) {	// if the letters are all writed, go to the second word
-		count++;
-		index = 0
-	}
-
-	setTimeout(typeWritter, 200)
 }
-
-typeWritter();
-
-function animeScroll() {
-	const windowTop = window.pageYOffset + (window.innerHeight * 0.60);
-
-	target.forEach(function(elem) {
-		if ((windowTop) > elem.offsetTop) {
-			console.log(elem);
-			elem.classList.add('animate');
-		} else {
-			elem.classList.remove('animate');
-
-		}
-	})
-}
-
-setTimeout(window.addEventListener('scroll', function() {
-	animeScroll();
-}), 400)
