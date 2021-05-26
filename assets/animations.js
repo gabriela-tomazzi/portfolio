c72a0d09a7ce8603c3073b49b94ba46aae0c9b6d
gabriela-tomazzi <gabtomazzi@gmail.com>
2021-05-26 18:45:02 -0300

    redesign

---
+++ assets/animations.js
@@ -0,0 +1,19 @@
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll () {
+	const windowTop = window.pageYOffset + (window.innerHeight * 0.85);
+
+	target.forEach(function (element) {
+		if ((windowTop) > element.offsetTop) {
+			element.classList.add(animationClass);
+		} else {
+			element.classList.remove(animationClass);
+		}
+	});
+}
+
+setTimeout(window.addEventListener('scroll', function () {
+	animeScroll();
+}), 400);
+
